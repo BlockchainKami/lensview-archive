048ef6c40be64f22d19e159593ca55bd1741560b
anjaysahoo3 <anjaysahoo3@gmail.com>
2022-12-03 11:00:56 +0530

    DB fetch publication ID done

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -7,6 +7,7 @@
     import { createClient, STORAGE_KEY, authenticate as authenticateMutation, getChallenge, getDefaultProfile } from '../api'
     import {parseJwt} from "../utils.js";
     import PROFILE from "./profile-store.js"
+    import getLensContent from '../lensPublication'
 
 
     let connected = false;
@@ -77,9 +78,14 @@ query getUrlData($url: String) {
                 query: query,
                 variables: {url: url}
             })
-        }).then(res => res.json()).then(data => {
-            console.log("Comment : " + data)
-            console.log("Hello")
+        }).then(res => res.json()).then( async data => {
+
+            console.log("Data from Fetch: " + data)
+            const publicationIDs = data['data']['getURL']["publications"]
+            console.log("Publication ID: ",publicationIDs)
+
+            const response = await getLensContent(publicationIDs)
+            console.log("Response : " + JSON.stringify(response))
         })
     }
 
@@ -90,8 +96,7 @@ query getUrlData($url: String) {
     import LENSHUB from '../abi/lenshub.json';
     import {create} from "ipfs-http-client";
 
-    const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
-    const projectSecret = process.env.NEXT_PUBLIC_PROJECT_SECRET;
+
     // Create Base64 Object
     var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
 
@@ -118,19 +123,24 @@ query getUrlData($url: String) {
     })
 
     const uploadToIPFS = async () => {
-        const metaData = {
-            version: '2.0.0',
-            content: userEnteredContent,
-            description: userEnteredContent,
-            name: `Post by @${profile.handle}`,
-            external_url: `https://lenster.xyz/u/${profile.handle}`,
-            metadata_id: uuid(),
-            mainContentFocus: 'TEXT_ONLY',
-            attributes: [],
-            locale: 'en-US'
-        }
-        const added = await client.add(JSON.stringify(metaData))
-        const uri = `https://ipfs.infura.io/ipfs/${added.path}`
+        const client = makeStorageClient()
+        const cid = await client.put(makeFileObjects())
+        console.log('stored files with cid:', cid)
+        const uri = `${cid}.ipfs.w3s.link/metaData.json`
+        // const metaData = {
+        //     version: '2.0.0',
+        //     content: userEnteredContent,
+        //     description: userEnteredContent,
+        //     name: `Post by @${profile.handle}`,
+        //     external_url: `https://lenster.xyz/u/${profile.handle}`,
+        //     metadata_id: uuid(),
+        //     mainContentFocus: 'TEXT_ONLY',
+        //     attributes: [],
+        //     locale: 'en-US'
+        // }
+        // const added = await client.add(JSON.stringify(metaData))
+        // const uri = `https://ipfs.infura.io/ipfs/${added.path}`
+        console.log("URI : " + uri);
         return uri
     }
 
@@ -184,6 +194,47 @@ query getUrlData($url: String) {
         }
     }
 
+    import { Web3Storage } from 'web3.storage'
+
+    function getAccessToken () {
+        // If you're just testing, you can paste in a token
+        // and uncomment the following line:
+        // return 'paste-your-token-here'
+
+        // In a real app, it's better to read an access token from an
+        // environement variable or other configuration that's kept outside of
+        // your code base. For this to work, you need to set the
+        // WEB3STORAGE_TOKEN environment variable before you run your code.
+    }
+
+    function makeStorageClient () {
+        return new Web3Storage({ token: getAccessToken() })
+    }
+
+    function makeFileObjects () {
+        // You can create File objects from a Blob of binary data
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/Blob
+        // Here we're just storing a JSON object, but you can store images,
+        // audio, or whatever you want!
+        const metaData = {
+            version: '2.0.0',
+            content: userEnteredContent,
+            description: userEnteredContent,
+            name: `Post by @${profile.handle}`,
+            external_url: `https://lenster.xyz/u/${profile.handle}`,
+            metadata_id: uuid(),
+            mainContentFocus: 'TEXT_ONLY',
+            attributes: [],
+            locale: 'en-US'
+        }
+        const blob = new Blob([JSON.stringify(metaData)], { type: 'application/json' })
+
+        const files = [
+            new File(['contents-of-file-1'], 'plain-utf8.txt'),
+            new File([blob], 'metaData.json')
+        ]
+        return files
+    }
 
 </script>
 
